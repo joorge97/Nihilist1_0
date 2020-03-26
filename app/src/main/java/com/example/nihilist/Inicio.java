98f441e04c8c21f7f3f410d0d9306a706b980a77
joorge97 <j321997@gmail.com>
2020-03-26 17:02:15 +0100

    Falta:   -Funciona perfectamente el deslizamiento entre mensajes, falta recibirlos   -Implementar las opciones de usuario   -Implementar los usuarios

--- app/src/main/java/com/example/nihilist/Inicio.java
+++ app/src/main/java/com/example/nihilist/Inicio.java
@@ -1,7 +1,6 @@
 package com.example.nihilist;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
 
 import android.content.Context;
 import android.content.Intent;
@@ -13,6 +12,16 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class Inicio extends AppCompatActivity {
@@ -21,6 +30,7 @@ public class Inicio extends AppCompatActivity {
     ArrayAdapter adaptador;
     ArrayList<String> lista = new ArrayList<String>();
     ListView listView;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,7 @@ public class Inicio extends AppCompatActivity {
         refrescar=(Button)findViewById(R.id.refrescar);
 
         cargarPreferencias(usuario);
+        lista=cargarMensajes("https://sqliteludens.000webhostapp.com/connect/getmensajes.php?toid="+usuario.getText().toString());
 
         enviar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,32 +53,19 @@ public class Inicio extends AppCompatActivity {
             }
         });
 
+        // desarrollo de la bbdd
+        lista.add("HOLA");
         refrescar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast toast = Toast.makeText(v.getContext(), "Refrescando mensajes.", Toast.LENGTH_SHORT);
                 toast.show();
+                lista.clear();
+                lista=cargarMensajes("https://sqliteludens.000webhostapp.com/connect/getmensajes.php?toid="+usuario.getText().toString());
+                adaptador = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,lista);
+                listView.setAdapter(adaptador);
             }
         });
-
-        // desarrollo de la bbdd
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-        lista.add("HOLA");
-
         adaptador = new ArrayAdapter(this,android.R.layout.simple_list_item_1,lista);
         listView.setAdapter(adaptador);
 
@@ -81,7 +79,29 @@ public class Inicio extends AppCompatActivity {
         usuario.setText(user);
     }
 
-    private void cargarMensajes(){
-
+    private ArrayList<String> cargarMensajes(String URL){
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        lista.add("De:"+jsonObject.getString("name")+": "+jsonObject.getString("message"));
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
+        requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+        return lista;
     }
 }
